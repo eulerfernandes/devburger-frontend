240f83bdcdfbbaa03dd66b1791519ec4f384c04d
eulerfernandes <pedraeuler@gmail.com>
2025-01-17 19:25:35 -0300

    add: img

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Login } from './pages/Login'
+import GlobalStyle from './styles/globalStyles'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
+    <GlobalStyle />
   </StrictMode>,
 )
