0499c98c105bd6478cd1c3116124f6ca346a6a47
eulerfernandes <pedraeuler@gmail.com>
2025-01-16 23:59:35 -0300

    Estilos Globais com Styled-Components

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const globalStyle = createGlobalStyle`
+
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+}
+
+    button {
+        cursor: pointer;
+    }
+`;
+
+export default globalStyle;
