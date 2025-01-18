fc120284fa03c593fff29222924030dae1faa06e
eulerfernandes <pedraeuler@gmail.com>
2025-01-18 00:36:21 -0300

    Formulário com React Hook Form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,7 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
 import Logo from '../../assets/logo.svg';
 import { Button } from '../../components/Button';
 
@@ -11,6 +15,22 @@ import {
 } from './styles';
 
 export function Login() {
+  const schema = yup
+    .object({
+      email: yup.string().email().required(),
+      password: yup.string().min(6).required(),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
     <Container>
       <LeftContainer>
@@ -24,17 +44,17 @@ export function Login() {
           Acesse com seu <span>Login</span> e senha.
         </Title>
 
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <InputContainer>
             <label>Email</label>
-            <input type="email" />
+            <input type="email" {...register('email')} />
           </InputContainer>
 
           <InputContainer>
             <label>Senha</label>
-            <input type="password" />
+            <input type="password" {...register('password')} />
           </InputContainer>
-          <Button>Entra</Button>
+          <Button type="submit">Entra</Button>
         </Form>
         <p>
           Não possui conta? <a>Clique aqui.</a>
