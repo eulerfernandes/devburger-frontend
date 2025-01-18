9b99ae329c17e44270254bf8a7662f2c9a81b603
eulerfernandes <pedraeuler@gmail.com>
2025-01-18 20:59:37 -0300

    Validando o formulário de Login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -17,8 +17,14 @@ import {
 export function Login() {
   const schema = yup
     .object({
-      email: yup.string().email().required(),
-      password: yup.string().min(6).required(),
+      email: yup
+        .string()
+        .email('Digite um e-mail valido')
+        .required('O email é obrigatorio'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter peko menos 6 caracteres')
+        .required('Digite uma senha'),
     })
     .required();
 
@@ -48,11 +54,13 @@ export function Login() {
           <InputContainer>
             <label>Email</label>
             <input type="email" {...register('email')} />
+            <p>{errors?.email?.message}</p>
           </InputContainer>
 
           <InputContainer>
             <label>Senha</label>
             <input type="password" {...register('password')} />
+            <p>{errors?.password?.message}</p>
           </InputContainer>
           <Button type="submit">Entra</Button>
         </Form>
